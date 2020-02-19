5ec1f6d92e0e71346c4d9acf21334e47c030b0fe
Imran Khan <yadav.vikas.eng@gmail.com>
2020-02-19 17:10:02 +0530

    New Add version 2.0.0

--- calc/calc.go
+++ calc/calc.go
@@ -1,11 +1,16 @@
 package calc
 
+import "errors"
+
 // Given a range of numbers it returns the
 // sum of those numbers
-func Add(nums ...int) int {
+func Add(nums ...int) (error, int) {
+	if len(nums) < 2 {
+		return errors.New("There should be at least 2 arguments"), 0
+	}
 	total := 0
 	for _, num := range nums {
 		total += num
 	}
-	return total
+	return nil, total
 }
